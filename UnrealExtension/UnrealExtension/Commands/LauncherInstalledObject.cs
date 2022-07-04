f9f6ef50322c75a3c157b081f3374fe30616e3e4
Josai <K.schaefer92@gmx.de>
2022-07-04 13:00:12 +0200

    Fix - Generate project files did not work for launcher versions
    Also added support for projects run by UE source solution directly.

---
+++ UnrealExtension/UnrealExtension/Commands/LauncherInstalledObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnrealExtension.Commands
+{
+    public class Installation
+    {
+        public string InstallLocation { get; set; }
+        public string NamespaceId { get; set; }
+        public string ItemId { get; set; }
+        public string ArtifactId { get; set; }
+        public string AppVersion { get; set; }
+        public string AppName { get; set; }
+    }
+    public class LauncherInstalledObject
+    {
+        public List<Installation> InstallationList { get; set; } = new List<Installation>();
+    }
+}
